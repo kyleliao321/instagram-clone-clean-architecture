5bed307bbaad1d1c0e21461cf32ffc276969f50b
kyleliao321 <kyleliao032@gmail.com>
2020-08-11 00:19:07 +0800

    Define android configuration and Module Dependency in buildSrc

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,25 @@
+object AndroidConfig {
+    const val COMPILE_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 23
+    const val TARGET_SDK_VERSION = 30
+    const val BUILD_TOOLS_VERSION = "30.0.0"
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+    const val ID = "com.example.instagram_clone_clean_architecture"
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+interface BuildType {
+
+    companion object {
+        const val RELEASE = "release"
+    }
+
+    val isMinifyEnabled: Boolean
+}
+
+object BuildTypeRelease : BuildType {
+    override val isMinifyEnabled = false
+}
---
+++ buildSrc/src/main/java/ModuleDependency.kt
@@ -0,0 +1,18 @@
+import kotlin.reflect.full.memberProperties
+
+internal const val FEATURE_PREFIX = ":feature_"
+
+object ModuleDependency {
+    const val APP = ":app"
+    const val FEATURE_PROFILE = ":feature_profile"
+    const val LIBRARY_BASE = ":library_base"
+
+    fun getAllModules() = ModuleDependency::class.memberProperties
+        .filter { it.isConst } // only const values are the module names
+        .map { it.getter.call().toString() } // get the name by calling getter
+        .toSet()
+
+    fun getDynamicFeatureModules() = getAllModules()
+        .filter { it.startsWith(FEATURE_PREFIX) }
+        .toSet()
+}
