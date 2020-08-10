05485f8ea74b395c70d36d22475785ddbf150938
kyleliao321 <kyleliao032@gmail.com>
2020-08-10 14:36:02 +0800

    Add profile edit view layout

--- feature_profile/src/main/java/com/example/instagram_clone_clean_architecture/feature/profile/presentation/view/edit/ProfileEditFragment.kt
+++ feature_profile/src/main/java/com/example/instagram_clone_clean_architecture/feature/profile/presentation/view/edit/ProfileEditFragment.kt
@@ -4,20 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.feature_profile.databinding.FragmentProfileEditBinding
 import com.example.library_base.presentation.fragment.InjectionFragment
 import org.kodein.di.instance
+import timber.log.Timber
 
 class ProfileEditFragment: InjectionFragment() {
 
     private val viewModel: ProfileEditViewModel by instance()
+
+    private lateinit var binding: FragmentProfileEditBinding
+
+    private val observer = Observer<ProfileEditViewModel.ViewState> {
+        Timber.d(it.toString())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentProfileEditBinding.inflate(inflater, container, false)
+        binding = FragmentProfileEditBinding.inflate(inflater, container, false)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.stateLiveData.observe(viewLifecycleOwner, observer)
+
+        viewModel.loadData()
     }
 }
