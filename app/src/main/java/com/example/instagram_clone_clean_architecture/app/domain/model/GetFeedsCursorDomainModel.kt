a42bc1340a1afa2196171953a78293932af2ec85
kyleliao321 <kyleliao032@gmail.com>
2021-01-13 21:42:44 +0800

    add feeds paging source

--- app/src/main/java/com/example/instagram_clone_clean_architecture/app/domain/model/GetFeedsCursorDomainModel.kt
+++ app/src/main/java/com/example/instagram_clone_clean_architecture/app/domain/model/GetFeedsCursorDomainModel.kt
@@ -3,6 +3,6 @@ package com.example.instagram_clone_clean_architecture.app.domain.model
 data class GetFeedsCursorDomainModel(
     val userId: String,
     val pageSize: Int,
-    val previous: String?,
-    val next: String?
+    val previous: String? = null,
+    val next: String? = null
 )
---
+++ feature_feeds/src/main/java/com/example/instagram_clone_clean_architecture/feature/feeds/data/page_source/FeedsPagingSource.kt
@@ -0,0 +1,89 @@
+package com.example.instagram_clone_clean_architecture.feature.feeds.data.page_source
+
+import androidx.paging.PagingSource
+import com.example.instagram_clone_clean_architecture.app.domain.data_source.RemoteDataSource
+import com.example.instagram_clone_clean_architecture.app.domain.model.GetFeedsCursorDomainModel
+import com.example.instagram_clone_clean_architecture.app.domain.model.PostDomainModel
+import com.example.library_base.domain.utility.Either
+import java.net.SocketTimeoutException
+
+class FeedsPagingSource(
+    private val remoteDataSource: RemoteDataSource,
+    private val query: Query
+) : PagingSource<FeedsPagingSource.Cursor, PostDomainModel>() {
+
+    override suspend fun load(params: LoadParams<Cursor>): LoadResult<Cursor, PostDomainModel> =
+        when (val key = params.key) {
+            is Cursor.NextCursor -> getNextFeeds(key, query)
+            is Cursor.PreviousCursor -> getPreviousFeeds(key, query)
+            null -> getLatestFeeds(query)
+        }
+
+    private suspend fun getLatestFeeds(query: Query): LoadResult<Cursor, PostDomainModel> {
+        val cursor = GetFeedsCursorDomainModel(query.userId, query.pageSize)
+        val getFeedsResult = remoteDataSource.getFeeds(cursor)
+
+        return when (getFeedsResult) {
+            is Either.Failure -> LoadResult.Error(SocketTimeoutException())
+            is Either.Success -> {
+                val feeds = getFeedsResult.a
+                LoadResult.Page(
+                    data = feeds,
+                    prevKey = null,
+                    nextKey = if (feeds.isEmpty()) null else Cursor.NextCursor(feeds.last().date)
+                )
+            }
+        }
+    }
+
+    private suspend fun getNextFeeds(
+        key: Cursor.NextCursor,
+        query: Query
+    ): LoadResult<Cursor, PostDomainModel> {
+        val cursor = GetFeedsCursorDomainModel(query.userId, query.pageSize, next = key.after)
+        val getFeedsResult = remoteDataSource.getFeeds(cursor)
+
+        return when (getFeedsResult) {
+            is Either.Failure -> LoadResult.Error(SocketTimeoutException())
+            is Either.Success -> {
+                val feeds = getFeedsResult.a
+                LoadResult.Page(
+                    data = feeds,
+                    prevKey = if (feeds.isEmpty()) Cursor.PreviousCursor(key.after) else Cursor.PreviousCursor(
+                        feeds.first().date
+                    ),
+                    nextKey = if (feeds.isEmpty()) null else Cursor.NextCursor(feeds.last().date)
+                )
+            }
+        }
+    }
+
+    private suspend fun getPreviousFeeds(
+        key: Cursor.PreviousCursor,
+        query: Query
+    ): LoadResult<Cursor, PostDomainModel> {
+        val cursor = GetFeedsCursorDomainModel(query.userId, query.pageSize, previous = key.before)
+        val getFeedsResult = remoteDataSource.getFeeds(cursor)
+
+        return when (getFeedsResult) {
+            is Either.Failure -> LoadResult.Error(SocketTimeoutException())
+            is Either.Success -> {
+                val feeds = getFeedsResult.a
+                LoadResult.Page(
+                    data = feeds,
+                    prevKey = if (feeds.isEmpty()) null else Cursor.PreviousCursor(feeds.first().date),
+                    nextKey = if (feeds.isEmpty()) Cursor.NextCursor(key.before) else Cursor.NextCursor(
+                        feeds.last().date
+                    )
+                )
+            }
+        }
+    }
+
+    data class Query(val userId: String, val pageSize: Int)
+
+    sealed class Cursor {
+        data class PreviousCursor(val before: String) : Cursor()
+        data class NextCursor(val after: String) : Cursor()
+    }
+}
