534b2d48ecd46f9c4501b90a3d78b6e44d74c630
kyleliao321 <kyleliao032@gmail.com>
2021-01-13 22:15:12 +0800

    add feed repository and get feeds flow usercase

---
+++ feature_feeds/src/main/java/com/example/instagram_clone_clean_architecture/feature/feeds/data/repository/FeedRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.example.instagram_clone_clean_architecture.feature.feeds.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.instagram_clone_clean_architecture.app.domain.data_source.RemoteDataSource
+import com.example.instagram_clone_clean_architecture.app.domain.model.PostDomainModel
+import com.example.instagram_clone_clean_architecture.feature.feeds.data.page_source.FeedsPagingSource
+import com.example.instagram_clone_clean_architecture.feature.feeds.domain.repository.FeedRepository
+import kotlinx.coroutines.flow.Flow
+
+class FeedRepositoryImpl(
+    private val remoteDataSource: RemoteDataSource
+) : FeedRepository {
+
+    override suspend fun getFeedsFlow(
+        userId: String,
+        pageSize: Int
+    ): Flow<PagingData<PostDomainModel>> {
+        val query = FeedsPagingSource.Query(userId, pageSize)
+        return Pager(
+            config = PagingConfig(
+                pageSize = query.pageSize,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { FeedsPagingSource(remoteDataSource, query) }
+        ).flow
+    }
+}
---
+++ feature_feeds/src/main/java/com/example/instagram_clone_clean_architecture/feature/feeds/domain/repository/FeedRepository.kt
@@ -0,0 +1,9 @@
+package com.example.instagram_clone_clean_architecture.feature.feeds.domain.repository
+
+import androidx.paging.PagingData
+import com.example.instagram_clone_clean_architecture.app.domain.model.PostDomainModel
+import kotlinx.coroutines.flow.Flow
+
+interface FeedRepository {
+    suspend fun getFeedsFlow(userId: String, pageSize: Int): Flow<PagingData<PostDomainModel>>
+}
